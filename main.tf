f26f7e59c19649ed4741e9b05e9ceeaded492101
sudipto92 <sudiptodasec@gmail.com>
2022-04-24 10:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,13 +21,6 @@ terraform {
 resource "aws_s3_bucket" "onebucket" {
    bucket = "sudo-testing-s3-with-terraform"
    acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Bucket1"
-     Environment = "Test"
-   }
 }
 
 
